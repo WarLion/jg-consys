e387409a70c72d8e5da13559480e3451fc095bc3
Juliet Garcia <juliet.garcia@outlook.com>
2013-05-19 13:42:55 -0430

    Has been created new files to show main page.

---
+++ application/views/index.blade.php
@@ -0,0 +1,253 @@
+@layout('layouts/default')
+
+@section('content')
+
+<div class="container">
+
+		<div class="row"> 
+			
+			<!-- Breadcrumb and Title -->
+			<div class="span12">
+
+				<h1 class="no-margin">Perfeto</h1>
+				<p class="lead">it's just a site templates</p>
+
+			</div>
+
+	</div>
+
+		<!-- Slider -->
+		<div class="slides">
+
+			<div><a href="#"><img src="img/example-slide-1.jpg" alt="Photo" /></a>
+				
+				<div class="detail">
+
+					<h3><a href="#">Cyril Hahn</a></h3>
+					<em>April 20th, 2013</em>          
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam <strong>nonummy nibh euismod</strong> <a href="#" class="view">...continue reading</a></p>
+					<a class="btn" href="#">View</a></div>
+		 
+			</div>
+
+			<div><a href="#"><img src="img/example-slide-2.jpg" alt="Photo" /></a>
+				
+				<div class="detail">
+					
+					<h3><a href="#">RA Loves Lea Peckre</a></h3>
+					<em>April 20th, 2013</em>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam <strong>nonummy nibh euismod</strong> <a href="#" class="view">...continue reading</a></p>
+					<a class="btn" href="#">View</a>
+
+				</div>
+			
+			</div>
+			
+			<div><a href="#"><img src="img/example-slide-3.jpg" alt="Photo" /></a>
+				
+				<div class="detail">
+					
+					<h3><a href="#">The Box Boutique’s New Shoe Classic</a></h3>
+					<em>April 20th, 2013</em>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam <strong>nonummy nibh euismod</strong> <a href="#" class="view">...continue reading</a></p>
+					<a class="btn" href="#">View</a>
+
+				</div>
+		 
+			</div>
+			
+			<div><a href="#"><img src="img/example-slide-4.jpg" alt="Photo" /></a>
+				
+				<div class="detail">
+					
+					<h3><a href="#">Craig-Lawrence</a></h3>
+					<em>April 20th, 2013</em>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam <strong>nonummy nibh euismod</strong> <a href="#" class="view">...continue reading</a></p>
+					<a class="btn" href="#">View</a>
+
+				</div>
+			
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<!-- Wide Banner -->
+<div class="wide-bg">
+	<div class="container">
+		<div class="row">
+			<div class="span3">
+				<h1 class="no-margin">Perfeto</h1>
+				<p class="lead no-margin">it's just a site templates</p>
+			</div>
+			<div class="span6"><em>Lorem ipsum dolor sit amet, <a href="#">consectetuer adipiscing</a> elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.<br />
+				<br />
+				Duis autem vel eum iriure <strong>dolor in hendrerit</strong> in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit.</em></div>
+			<div class="span3"><a href="#" class="btn btn-large btn-primary margin">Buy it</a></div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</div>
+
+		<hr />
+		
+<div class="section">
+
+  <div class="container">
+
+		<!-- Latest Works -->
+		<div class="row">
+
+			<div class="span12">
+
+				<p class="lead">Latest Works</p>
+				<ul class="thumbnails">
+					<li class="span3">
+						
+						<div class="thumbnail"><a href="img/example-slide-1.jpg" title="Box Boutique" rel="thumbnails" class="thumb fancybox"><img src="img/example-slide-1.jpg" alt="Photo" /> <span class="view"><i class="icon-search icon-white"></i></span></a>
+							
+							<div class="caption">
+								<h3><a href="#">Cyril Hahn</a></h3>
+								<em>April 20th, 2013</em>
+								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam <strong>nonummy nibh euismod</strong>.</p>
+								<a class="btn" href="#">View</a>
+
+							</div>
+
+						</div>
+
+					</li>
+
+					<li class="span3">
+						
+						<div class="thumbnail"><a href="img/example-slide-2.jpg" title="Box Boutique" rel="thumbnails" class="thumb fancybox"><img src="img/example-slide-2.jpg" alt="Photo" /> <span class="view"><i class="icon-search icon-white"></i></span></a>
+							
+							<div class="caption">
+
+								<h3><a href="#">Cyril Hahn</a></h3>
+								<em>April 20th, 2013</em>
+								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam <strong>nonummy nibh euismod</strong>.</p>
+								<a class="btn" href="#">View</a>
+
+							</div>
+
+						</div>
+
+					</li>
+
+					<li class="span3">
+						
+						<div class="thumbnail"><a href="img/example-slide-3.jpg" title="Box Boutique" rel="thumbnails" class="thumb fancybox"><img src="img/example-slide-3.jpg" alt="Photo" /> <span class="view"><i class="icon-search icon-white"></i></span></a>
+							
+							<div class="caption">
+
+								<h3><a href="#">Cyril Hahn</a></h3>
+								<em>April 20th, 2013</em>
+								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam <strong>nonummy nibh euismod</strong>.</p>
+								<a class="btn" href="#">View</a>
+
+							</div>
+						
+						</div>
+
+					</li>
+
+					<li class="span3">
+
+						<div class="thumbnail"><a href="img/example-slide-4.jpg" title="Box Boutique" rel="thumbnails" class="thumb fancybox"><img src="img/example-slide-4.jpg" alt="Photo" /> <span class="view"><i class="icon-search icon-white"></i></span></a>
+						 
+							<div class="caption">
+
+								<h3><a href="#">Cyril Hahn</a></h3>
+								<em>April 20th, 2013</em>
+								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam <strong>nonummy nibh euismod</strong>.</p>
+								<a class="btn" href="#">View</a>
+
+							</div>
+
+						</div>
+
+					</li>
+
+				</ul>
+
+			</div>
+
+		</div>
+
+		<hr />
+
+		<div class="row"> 
+			
+			<!-- Featured Posts -->
+			<div class="span4">
+
+				<h3 class="margin">Featured Posts</h3>
+				<ul class="media-list">
+					
+					<li class="media simple"><a href="img/example-slide-1.jpg" title="Box Boutique" rel="recent" class="pull-left thumb fancybox"><img src="img/example-slide-1.jpg" alt="Photo" /> <span class="view"><i class="icon-search icon-white"></i></span></a>
+						
+						<div class="media-body">
+							<h4 class="media-heading half-margin"><a href="#">Scelerisque ante sollicitudin commodo</a></h4>
+							<a href="#" class="btn btn-small">View</a>
+
+						</div>
+
+					</li>
+
+				</ul>
+
+			</div>
+
+			<div class="span4">
+
+				<h3 class="margin">Featured Posts</h3>
+
+				<ul class="media-list">
+
+					<li class="media simple"><a href="img/example-slide-2.jpg" title="Box Boutique" rel="recent" class="pull-left thumb fancybox"><img src="img/example-slide-2.jpg" alt="Photo" /> <span class="view"><i class="icon-search icon-white"></i></span></a>
+						
+						<div class="media-body">
+
+							<h4 class="media-heading half-margin"><a href="#">Scelerisque ante sollicitudin commodo</a></h4>
+							<a href="#" class="btn btn-small">View</a>
+
+						</div>
+
+					</li>
+
+				</ul>
+
+			</div>
+
+			<div class="span4">
+
+				<h3 class="margin">Most Commented</h3>
+
+				<ul class="media-list">
+
+					<li class="media simple"><a href="img/example-slide-3.jpg" title="Box Boutique" rel="recent" class="pull-left thumb fancybox"><img src="img/example-slide-3.jpg" alt="Photo" /> <span class="view"><i class="icon-search icon-white"></i></span></a>
+						
+						<div class="media-body">
+							
+							<h4 class="media-heading half-margin"><a href="#">Scelerisque ante sollicitudin commodo</a></h4>
+							<a href="#" class="btn btn-small">View</a>
+
+						</div>
+
+					</li>
+
+				</ul>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+@endsection
---
+++ application/views/layouts/default.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+	<!-- Mobile Devices -->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Urbanización Villas de Aragua</title>
+
+	{{ Asset::styles() }}
+
+</head>
+
+<body>
+
+<div class="section">
+
+	<div class="container"> 
+		
+		<!-- Header -->
+		<div class="row">
+
+			<div class="span6">
+
+				<p class="logo"><a href="#"><span>Perfetto</span></a></p>
+
+			</div>
+
+			<div class="span6">
+
+				<div class="top-header text-right"><em>call us (123) 456 7890 - <a href="#">my@mail.com</a></em>
+
+					<div class="social">
+						<a href="#" class="ico-rss"><img src="img/rss-icon.png" alt="Rss" /></a> 
+						<a href="#" class="ico-flickr"><img src="img/flickr-icon.png" alt="Flickr" /></a> 
+						<a href="#" class="ico-google-plus"><img src="img/google-plus-icon.png" alt="Google Plus" /></a> 
+						<a href="#" class="ico-twitter"><img src="img/twitter-icon.png" alt="Twitter" /></a> 
+						<a href="#" class="ico-facebook"><img src="img/facebook-icon.png" alt="Facebook" /></a>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+		
+		<!-- Navbar -->
+		<div class="navbar" id="nav-follow">
+			
+			<div class="navbar-inner">
+				
+				<div class="container">
+
+					<a data-target=".navbar-responsive-collapse" data-toggle="collapse" class="btn btn-navbar"><i class="icon-th-list"></i></a> 
+					<a data-target=".navbar-responsive-collapse" data-toggle="collapse" class="brand">Navbar</a>
+					
+					<div class="nav-collapse collapse navbar-responsive-collapse">
+						
+						<ul class="nav">
+							
+							<li class="dropdown active">{{ HTML::link('index','Inicio',array('title' => 'Inicio')) }} <b class="caret"></b> <span>Página principal</span></li>
+
+							<li class="dropdown active">{{ HTML::link('nosotros/index','La urbanización',array('title' => 'La urbanización')) }}<b class="caret"></b> <span>Villas de Aragua</span>
+								
+								<ul>
+									
+									<li>{{ HTML::link('nosotros/historia','Historia',array('title' => 'Historia')) }}</li>
+									<li>{{ HTML::link('nosotros/filosofia','Filosofia',array('title' => 'Filosofia')) }}</li>
+									<li>{{ HTML::link('nosotros/galeria','Galería de fotos',array('title' => 'Galería de fotos')) }}</li>
+									<li>{{ HTML::link('nosotros/condominio','Junta de condominio',array('title' => 'Junta de condominio')) }}</li>
+								
+								</ul>
+							
+							</li>              
+						 
+							<li>{{ HTML::link('/','Información',array('title' => 'Información')) }} <span>--</span>
+
+								<ul>
+									
+									<li>{{ HTML::link('informacion/reuniones','Asambleas y reuniones',array('title' => 'Asambleas y reuniones')) }}</li>
+									<li>{{ HTML::link('informacion/partidas','Egresos e ingresos',array('title' => 'Egresos e ingresos')) }}</li>
+									<li>{{ HTML::link('informacion/documentos','Documentos',array('title' => 'Documentos')) }}</li>
+									<li>{{ HTML::link('informacion/proyectos','Proyectos',array('title' => 'Proyectos')) }}</li>
+								
+								</ul>
+
+							</li>
+							
+							<li class="dropdown">{{ HTML::link('foro/index','Foro',array('title' => 'Foro')) }}<b class="caret"></b> <span>Comunidad</span></li>
+
+							<li class="dropdown">{{ HTML::link('contacto/index','Contacto',array('title' => 'Contacto')) }}<b class="caret"></b> <span>Comuníquese con nosotros</span></li>            
+						</ul>
+					
+					</div>
+				
+				</div>
+			
+			</div>
+		
+		</div>
+		
+		<hr />
+	
+	</div>
+
+	@yield('content')
+
+	<div class="section">
+
+		<div class="container"> 
+		
+			<div class="row">
+
+				<div class="span12">
+
+					<div class="footer">
+
+						<hr />
+
+						<p class="text-center">&copy; 2013 <strong>Sigcon - {{ HTML::mailto('juliet.garcia@outlook.com','Juliet Garcia' ) }}</strong><br />Todos los derechos reservados.</p>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	<!-- Go to top page -->
+	<div id="toTop">Top</div>
+
+		{{ Asset::scripts() }}
+
+		@yield('postscript')
+
+</body>
+
+</html>
