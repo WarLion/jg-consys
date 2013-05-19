aa79455030835750a39f9f8d08be8c0e69ff5944
Juliet Garcia <juliet.garcia@outlook.com>
2013-05-19 13:51:18 -0430

    Has been created a font file and added style sheet and javascripts files to show in the head.

--- application/controllers/base.php
+++ application/controllers/base.php
@@ -2,6 +2,29 @@
 
 class Base_Controller extends Controller {
 
+	public function __construct() {
+
+		// Styles
+		Asset::add('bootstrap','css/bootstrap.min.css');
+		Asset::add('bootstrap-responsive','css/bootstrap-responsive.min.css');
+		Asset::add('styles','css/style.css');
+		Asset::add('slides','css/slides.css');
+		Asset::add('masonry','css/masonry.css');
+		Asset::add('fancybox','css/fancybox.css');
+		Asset::add('custom','css/colors/custom.css');
+		Asset::add('water','css/colors/water.css');
+		Asset::add('font_oswald','css/fonts/font_oswald.css');
+
+		// Javascripts files
+		Asset::add('jquery','js/jquery.js');
+		Asset::add('bootstrap','js/bootstrap.min.js');
+		Asset::add('slides','js/slides.min.js');
+		Asset::add('masonry','js/masonry.min.js');
+		Asset::add('fancybox','js/fancybox.js');
+		Asset::add('functions','js/functions.js');
+
+	}
+
 	/**
 	 * Catch-all method for requests that can't be matched.
 	 *
