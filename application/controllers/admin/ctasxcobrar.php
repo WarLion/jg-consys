f3abde4622e19b1975d3bed5a36e77975d0fa639
Juliet Garcia <juliet.garcia@outlook.com>
2013-06-25 11:17:17 -0430

    New controller for functions the module accounts receivable.

---
+++ application/controllers/admin/ctasxcobrar.php
@@ -0,0 +1,45 @@
+<?php
+class Admin_Ctasxcobrar_Controller extends Base_Controller {
+
+	public $restful = true;
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function get_index()
+	{
+		$title = 'Cuentas por Cobrar - Sistema Administrativo JG-Sigcon';
+		return View::make('administracion.ctasxcobrar.index')->with('title',$title);
+	}
+
+	public function get_agregar()
+	{
+		$title = 'Agregar - Cuentas por Cobrar - Sistema Administrativo JG-Sigcon';
+		return View::make('administracion.ctasxcobrar.agregar')->with('title',$title);
+	}
+
+	public function get_detalle()
+	{
+		$title = 'Detalle - Cuentas por Cobrar - Sistema Administrativo JG-Sigcon';
+		return View::make('administracion.ctasxcobrar.detalle')->with('title',$title);
+	}
+
+	public function post_index()
+	{
+		
+	}
+
+	public function post_agregar()
+	{
+		
+	}
+
+	public function post_detalle()
+	{
+		
+	}
+
+}
+
+?>
