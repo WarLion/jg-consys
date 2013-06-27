07ba79e6b6db4c1e66603cb851bcbe6bc5dbc559
Juliet Garcia <juliet.garcia@outlook.com>
2013-06-27 16:00:03 -0430

    Has been deleted frame because it not used.

--- application/views/administracion/ctasxpagar/documento.blade.php
+++ application/views/administracion/ctasxpagar/documento.blade.php
@@ -26,109 +26,69 @@
 					<label class="control-label" for="inputProveedor"><strong>Tipos de documento</strong></label>
 					<div class="controls">
 
-						<input type="text" id="proveedor" name="proveedor" placeholder="Proveedor"><br>
+						<input type="text" id="proveedor" name="proveedor" placeholder="Proveedor">
 						<button type="submit" class="btn btn-primary">Agregar</button>
 
 					</div>
 
 				</div>
 
+				<hr>
+
+				<strong>Documentos</strong>
+
 				<hr class="bs-docs-separator">
 
-			<div class="row-fluid">
+				<div class="row-fluid">	
 
-				<div class="span6">
+					<div class="span6">
 
-					<p><strong>Documento</strong></p> 
+						<div class="well">
 
-					<hr class="bs-docs-separator">
+						    <table class="table table-hover">
 
-					<div class="well">
+						      <thead>
+						        <tr>
+						          <th>#</th>
+						          <th>Descripción</th>
+						          <th>Opciones</th>
+						          <th style="width: 36px;"></th>
+						        </tr>
+						      </thead>
 
-					
+						      <tbody>
+						        <tr>
+						          <td>1</td>
+						          <td>Factura</td>
+						          <td><a href="#myModal" role="button" data-toggle="modal"><i class="icon-edit"></i></a>  <a href="#myModal" role="button" data-toggle="modal"><i class="icon-remove"></i></a></td>
+						        </tr>			        
+						      </tbody>
 
-						<div class="control-group">
-
-							<label class="control-label-right" for="inputParcela"><strong>Tipo</strong></label>
-							<div class="controls">
-
-								<select>
-
-									<option>1</option>
-									<option>2</option>
-									<option>3</option>
-									<option>4</option>
-									<option>5</option>
-
-								</select>
-
-							</div>
+						    </table>
 
 						</div>
 
-						<div class="control-group">
-							<label class="control-label-right" for="inputMonto"><strong>Monto</strong></label>
-							<div class="controls">
-								<input type="text" id="monto" name="monto" class="input-small" readonly>
-							</div>
-						</div>
+					</div>	
 
 				</div>
 
+				<div class="modal small hide fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				    
+				    <div class="modal-header">
+				        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				        <h3 id="myModalLabel">Delete Confirmation</h3>
+				    </div>
+				    
+				    <div class="modal-body">
+				        <p class="error-text">Are you sure you want to delete the user?</p>
+				    </div>
+				    
+				    <div class="modal-footer">
+				        <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+				        <button class="btn btn-danger" data-dismiss="modal">Delete</button>
+				    </div>
+				    
 				</div>
-
-			</div>
-
-			<a href="{{ URL::to('#'); }}" class="btn btn-primary">Agregar</a>
-
-			<div>&nbsp;</div>
-
-			<strong>Tipos de documento</strong>
-
-			<hr class="bs-docs-separator">			
-
-			<div class="well">
-
-			    <table class="table table-hover">
-
-			      <thead>
-			        <tr>
-			          <th>#</th>
-			          <th>Descripción</th>
-			          <th>Opciones</th>
-			          <th style="width: 36px;"></th>
-			        </tr>
-			      </thead>
-
-			      <tbody>
-			        <tr>
-			          <td>1</td>
-			          <td>Factura</td>
-			          <td><a href="#myModal" role="button" data-toggle="modal"><i class="icon-edit"></i></a>  <a href="#myModal" role="button" data-toggle="modal"><i class="icon-remove"></i></a></td>
-			        </tr>			        
-			      </tbody>
-
-			    </table>
-
-			</div>			
-
-			<div class="modal small hide fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-			    
-			    <div class="modal-header">
-			        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-			        <h3 id="myModalLabel">Delete Confirmation</h3>
-			    </div>
-			    
-			    <div class="modal-body">
-			        <p class="error-text">Are you sure you want to delete the user?</p>
-			    </div>
-			    
-			    <div class="modal-footer">
-			        <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
-			        <button class="btn btn-danger" data-dismiss="modal">Delete</button>
-			    </div>
-			    
-			</div>
 
 			</form>
 
