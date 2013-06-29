071b8f85133292625b73548357b77a3890c27132
Juliet Garcia <juliet.garcia@outlook.com>
2013-06-29 14:18:03 -0430

    For displaying concepts.

--- application/controllers/admin/ctasxcobrar.php
+++ application/controllers/admin/ctasxcobrar.php
@@ -16,7 +16,12 @@ class Admin_Ctasxcobrar_Controller extends Base_Controller {
 	public function get_agregar()
 	{
 		$title = 'Agregar - Cuentas por Cobrar - Sistema Administrativo JG-Sigcon';
-		return View::make('administracion.ctasxcobrar.agregar')->with('title',$title);
+
+		$conceptos = DB::table('tadm_conceptos')->get(array('nombre','monto'));
+
+		return View::make('administracion.ctasxcobrar.agregar')
+			->with('title',$title)
+			->with('conceptos',$conceptos);
 	}
 
 	public function get_detalle()
