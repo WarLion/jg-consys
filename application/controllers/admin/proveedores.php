c8af62b4ea2cb96a704abefc7619aa6d393f9b69
Juliet Garcia <juliet.garcia@outlook.com>
2013-07-08 23:23:18 -0430

    Added variable message and logic the method post_agregar().

--- application/controllers/admin/proveedores.php
+++ application/controllers/admin/proveedores.php
@@ -24,7 +24,10 @@ class Admin_proveedores_Controller extends Base_Controller {
 	public function get_agregar()
 	{
 		$title = 'Agregar - Proveedores - Sistema Administrativo JG-Sigcon';
-		return View::make('administracion.proveedores.agregar')->with('title',$title);
+		$message = null;
+		return View::make('administracion.proveedores.agregar')
+			->with('title',$title)
+			->with('message',$message);
 	}
 
 	public function get_detalle()
@@ -40,7 +43,54 @@ class Admin_proveedores_Controller extends Base_Controller {
 
 	public function post_agregar()
 	{
+		$title = 'Agregar - Proveedores - Sistema Administrativo JG-Sigcon';
+
+		$message = null;
 		
+		$rif 		= Input::get('rif');
+		$nombre 	= Input::get('nombre');
+		$telefono 	= Input::get('telefono');
+		$email 		= Input::get('email');
+		$direccion 	= Input::get('direccion');
+		$fecha 		= date("d-m-Y H:i:s");
+
+		$form = array($rif, $nombre, $telefono, $email, $direccion, $fecha);
+
+		foreach($form as $frm)
+		{		
+			if(empty($frm))
+			{
+				$message = "No puede dejar campos vacios.";
+				
+				return View::make('administracion.proveedores.agregar')
+					->with('title',$title)
+					->with('message',$message);
+			}
+		}
+
+		$proveedor = DB::table('tadm_proveedor')
+			->where('nro','=',$rif)
+			->get();
+
+		if(!empty($proveedor))
+		{
+			$message = "Proveedor ya existe.";
+		}
+		else
+		{
+			DB::table('tadm_proveedor')
+				->insert(array('nro' 			=> $rif,
+								'descripcion' 	=> $nombre,
+								'direccion' 	=> $direccion,
+								'telefono' 		=> $telefono,
+								'email' 		=> $email,
+								'fecha_ing' 	=> $fecha,
+								'activo' 		=> '1'));
+		}
+
+		return View::make('administracion.proveedores.agregar')
+			->with('title',$title)
+			->with('message',$message);
 	}
 
 	public function post_detalle()
