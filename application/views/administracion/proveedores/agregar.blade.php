dd88c3a278b01499e752f321be72e7abc7a9f469
Juliet Garcia <juliet.garcia@outlook.com>
2013-07-08 23:21:00 -0430

    Added has been register form and names.

--- application/views/administracion/proveedores/agregar.blade.php
+++ application/views/administracion/proveedores/agregar.blade.php
@@ -17,7 +17,7 @@
 
 			<hr>
 
-			<form class="form-modules">
+			<form action="{{ URL::to('admin/proveedores/agregar') }}" method="post" class="form-modules">
 
 				<div class="well">
 
@@ -25,26 +25,28 @@
 
 						<div class="span6">
 
+							{{ $message; }}
+
 							<div class="control-group">
 
 								<label class="control-label-right" for="inputRif"><strong>RIF</strong></label>
 								<div class="controls">
 
-									<input type="text" id="rif" placeholder="rif">
+									<input type="text" name="rif" id="rif" placeholder="rif">
 
 								</div>
 
 								<label class="control-label-right" for="inputNombre"><strong>Nombre</strong></label>
 								<div class="controls">
 
-									<input type="text" id="nombre" placeholder="Nombre">
+									<input type="text" name="nombre" id="nombre" placeholder="Nombre">
 
 								</div>
 
 								<label class="control-label-right" for="inputTelefono"><strong>Teléfono</strong></label>
 								<div class="controls">
 
-									<input type="text" id="telefono" placeholder="Teléfono">
+									<input type="text" name="telefono" id="telefono" placeholder="Teléfono">
 
 								</div>
 
@@ -59,14 +61,14 @@
 								<label class="control-label-right" for="inputEmail"><strong>Correo Electrónico</strong></label>
 								<div class="controls">
 
-									<input type="text" id="inputEmail" placeholder="Correo Electrónico">
+									<input type="text" name="email" id="inputEmail" placeholder="Correo Electrónico">
 
 								</div>
 
 								<label class="control-label-right" for="inputDireccion"><strong>Dirección</strong></label>
 								<div class="controls">
 
-									<input type="text" id="Direccion" placeholder="Dirección">
+									<input type="text" name="direccion" id="Direccion" placeholder="Dirección">
 
 								</div>
 
@@ -78,7 +80,7 @@
 
 				</div>
 
-				<a href="{{ URL::to('#'); }}" class="btn btn-primary">Agregar</a>
+				<input type="submit" class="btn btn-primary" name="add" value="Agregar">
 
 			</form>
 
