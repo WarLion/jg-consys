7d90cb93d286639de5d4611a50d2e320eeb40b8e
Juliet Garcia <juliet.garcia@outlook.com>
2013-07-11 01:31:48 -0430

    Added views and controller for displaying information about propietarios.

--- application/controllers/admin/propietarios.php
+++ application/controllers/admin/propietarios.php
@@ -32,7 +32,11 @@ class Admin_propietarios_Controller extends Base_Controller {
 	public function get_agregar()
 	{
 		$title = 'Agregar - Sistema Administrativo JG-Sigcon';
-		return View::make('administracion.propietarios.agregar')->with('title',$title);
+		$message = null;
+
+		return View::make('administracion.propietarios.agregar')
+			->with('title',$title)
+			->with('message',$message);
 	}
 
 	public function post_index()
@@ -47,7 +51,65 @@ class Admin_propietarios_Controller extends Base_Controller {
 
 	public function post_agregar()
 	{
-		
+		$title = 'Agregar - Sistema Administrativo JG-Sigcon';
+		$message = null;
+
+		$cedula 	= Input::get('cedula');
+		$nombre 	= Input::get('nombre');
+		$sexo 		= Input::get('sexo');
+		$celular 	= Input::get('celular');
+		$casa 		= Input::get('casa');
+		$email 		= Input::get('email');
+		$parcela 	= Input::get('parcela');
+		$fechan 	= Input::get('fechan');
+		$fechai 	= date("d-m-Y H:i:s");
+
+		$form = array($cedula,$nombre,$parcela);
+
+		// verifica que los campos no esten vacios
+		foreach($form as $frm)
+		{		
+			if(empty($frm))
+			{
+				$message = "No puede dejar campos vacios.";
+				
+				return View::make('administracion.propietarios.agregar')
+					->with('title',$title)
+					->with('message',$message);
+			}
+		}
+
+		$propietarios = DB::table('tadm_propietarios')
+			->where('ci','=',$cedula)
+			->get();
+
+		if(empty($propietarios))
+		{
+			$message = "Propietario se ha registrado satisfactoriamente.";
+
+			DB::table('tadm_propietarios')
+				->insert(array('ci' 		=> $cedula,
+								'nombre' 	=> $nombre,
+								'sexo'		=> $sexo,
+								'tlf_cel' 	=> $celular,
+								'tlf_casa'	=> $casa,
+								'email'		=> $email,
+								'fecha_nac'	=> $fechan,
+								'fecha_ing' => $fechai,
+								'actual' 	=> "1"));
+
+			DB::table('tadm_agrupar')
+				->insert(array('propietarios_ci'	=> $cedula,
+								'parcela_nro' 		=> $parcela));
+		}
+		else
+		{
+			$message = "Propietario ya existe.";
+		}
+
+		return View::make('administracion.propietarios.agregar')
+			->with('title',$title)
+			->with('message',$message);
 	}
 }
 
--- application/views/administracion/propietarios/agregar.blade.php
+++ application/views/administracion/propietarios/agregar.blade.php
@@ -17,41 +17,48 @@
 
 			<div>&nbsp;</div>
 
-			<form class="form-modules">
+			<form action="{{ URL::to('admin/propietarios/agregar') }}" method="post" class="form-modules">
 
 				<div class="row-fluid">
 
 					<div class="span6">
+
+						{{ $message }}
 
 						<div class="control-group">
 
 							<label class="control-label-right" for="inputCedula"><strong>Cédula</strong></label>
 							<div class="controls">
 
-								<input type="text" id="inputCedula" placeholder="Cédula">
+								<input type="text" name="cedula" id="inputCedula" placeholder="Cédula">
 
 							</div>
 
 							<label class="control-label-right" for="inputNombres"><strong>Nombre(s)</strong></label>
 							<div class="controls">
 
-								<input type="text" id="inputNombres" placeholder="Nombre(s) y apellido(s)">
+								<input type="text" name="nombre" id="inputNombres" placeholder="Nombre(s) y apellido(s)">
+
+							</div>
+
+							<label class="control-label-right" for="inputTlfCasa"><strong>Sexo</strong></label>
+							<div class="controls">
+
+								<select name="sexo">
+									<option value="Femenino">Femenino</option>
+									<option value="Masculino">Masculino</option>
+								</select>
 
 							</div>
 
 							<label class="control-label-right" for="inputCelular"><strong>Tlf. Celular</strong></label>
 							<div class="controls">
 
-								<input type="text" id="inputCelular" placeholder="Teléfono celular">
+								<input type="text" name="celular" id="inputCelular" placeholder="Teléfono celular">
 
 							</div>
 
-							<label class="control-label-right" for="inputTlfCasa"><strong>Tlf. Casa</strong></label>
-							<div class="controls">
 
-								<input type="text" id="inputTlfCasa" placeholder="Teléfono casa">
-
-							</div>
 
 						</div>
 
@@ -61,24 +68,31 @@
 
 						<div class="control-group">
 
+							<label class="control-label-right" for="inputTlfCasa"><strong>Tlf. Casa</strong></label>
+							<div class="controls">
+
+								<input type="text" name="casa" id="inputTlfCasa" placeholder="Teléfono casa">
+
+							</div>
+
 							<label class="control-label-right" for="inputEmail"><strong>Correo Electrónico</strong></label>
 							<div class="controls">
 
-								<input type="text" id="inputEmail" placeholder="Correo Electrónico">
+								<input type="text" name="email" id="inputEmail" placeholder="Correo Electrónico">
 
 							</div>
 
 							<label class="control-label-right" for="inputParcela"><strong>Parcela</strong></label>
 							<div class="controls">
 
-								<input type="text" id="inputParcela" placeholder="Parcela">
+								<input type="text" name="parcela" id="inputParcela" placeholder="Parcela">
 
 							</div>
 
 							<label class="control-label-right" for="inputParcela"><strong>Fecha de nacimiento</strong></label>
 							<div class="controls">
 
-								<input type="text" id="inputParcela" placeholder="dd/mm/aaaa">
+								<input type="text" name="fechan" id="inputParcela" placeholder="dd/mm/aaaa">
 
 							</div>
 
@@ -88,7 +102,7 @@
 
 				</div>
 
-				<a href="{{ URL::to('#'); }}" class="btn btn-primary">Agregar</i></a>
+				<input type="submit" class="btn btn-primary" name="add" value="Agregar">
 
 			</form>
 
