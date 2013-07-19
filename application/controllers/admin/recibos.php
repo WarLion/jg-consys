b5bd554c85963f12eb2c5c86a734bfb42ba1bfcc
Juliet Garcia <juliet.garcia@outlook.com>
2013-07-19 06:15:08 -0430

    Logic for displaying all process of generating receipt.

--- application/controllers/admin/recibos.php
+++ application/controllers/admin/recibos.php
@@ -25,19 +25,27 @@ class Admin_recibos_Controller extends Base_Controller {
 	public function get_generar()
 	{
 		$title = 'Generar recibos - Recibos - Sistema Administrativo JG-Sigcon';
-		return View::make('administracion.recibos.generar')->with('title',$title);
+		$message = null;
+		$parcela = null;
+		$ci = null;
+		$name = null;
+
+		return View::make('administracion.recibos.generar')
+			->with('title',$title)
+			->with('parcela',$parcela)
+			->with('ci',$ci)
+			->with('name',$name)
+			->with('message',$message);
 	}
 
 	public function get_generar_pago()
 	{
-		$title = 'Forma de Pago - Recibos - Sistema Administrativo JG-Sigcon';
-		return View::make('administracion.recibos.generar_pago')->with('title',$title);
+		return Redirect::to('admin/recibos/generar');
 	}
 
 	public function get_generar_imprimir()
 	{
-		$title = 'Procesar - Recibos - Sistema Administrativo JG-Sigcon';
-		return View::make('administracion.recibos.generar_imprimir')->with('title',$title);
+		return Redirect::to('admin/recibos/generar');
 	}
 
 	public function get_detalle()
@@ -72,7 +80,145 @@ class Admin_recibos_Controller extends Base_Controller {
 
 	public function post_generar()
 	{
-		
+		$title = 'Generar recibos - Recibos - Sistema Administrativo JG-Sigcon';
+		$message = null;
+		$x = 1;
+
+		$parcela = Input::get('parcela');
+
+		// busca el nro de parcela para mostrar los datos del propietario
+		$propietario = DB::table('tadm_agrupar')
+			->where('parcela_nro','=',$parcela)
+			->join('tadm_propietarios','tadm_propietarios.ci','=','tadm_agrupar.propietarios_ci')
+			->get(array('tadm_agrupar.propietarios_ci','tadm_propietarios.nombre'));
+
+		// verifica si la busqueda es verdadera
+		if(empty($propietario)) 
+		{ 
+			$message = "El nro de parcela no existe.";
+		}
+		else
+		{
+			// Para mostrar las deudas actuales del propietario que se busca
+			$ctasxcobrar = DB::table('tadm_ctasxcobrar')
+				->where('parcela_nro','=',$parcela)
+				->join('tadm_conceptos','tadm_ctasxcobrar.concepto_codigo','=','tadm_conceptos.codigo')
+				->where('pagos_id','=','0')
+				->get(array('tadm_ctasxcobrar.concepto_codigo','tadm_conceptos.nombre','tadm_ctasxcobrar.fecha','tadm_ctasxcobrar.monto'));
+
+			// Suma el monto de los conceptos que debe el propietario
+			$sum_monto = DB::table('tadm_ctasxcobrar')
+				->where('parcela_nro','=',$parcela)
+				->join('tadm_conceptos','tadm_ctasxcobrar.concepto_codigo','=','tadm_conceptos.codigo')
+				->sum('tadm_ctasxcobrar.monto');
+
+			return View::make('administracion.recibos.generar')
+				->with('title',$title)
+				->with('x',$x)
+				->with('message',$message)
+				->with('parcela',$parcela)
+				->with('propietario',$propietario)
+				->with('ctasxcobrar',$ctasxcobrar)
+				->with('sum_monto',$sum_monto);
+		}
+
+		return View::make('administracion.recibos.generar')
+			->with('title',$title)
+			->with('message',$message);
+	}
+
+	public function post_generar_pago()
+	{
+		$title = 'Forma de Pago - Recibos - Sistema Administrativo JG-Sigcon';
+		$y = 0;
+
+		$generar = array('parcela' 	=> Input::get('hparcela'), // recibe valores de peticion anterior -- luego se cambiara por variables Session
+						'ci' 		=> Input::get('hci'),		// recibe valores de peticion anterior -- luego se cambiara por variables Session
+						'name' 		=> Input::get('hnombre'),	// recibe valores de peticion anterior -- luego se cambiara por variables Session
+						'monto' 	=> Input::get('monto')
+					);
+
+		// recibe todas las variables
+		$nro_conceptos = Input::get(); 
+
+		foreach($nro_conceptos as $ncon)
+		{
+			// se busca el nro de concepto
+			$concept_positivo = DB::table('tadm_ctasxcobrar')
+									->where('parcela_nro','=',$generar['parcela'])
+									->where('pagos_id','=','0')
+									->where('concepto_codigo','=',$ncon)
+									->get(array('concepto_codigo'));
+
+			// si el concepto existe
+			if(!empty($concept_positivo))
+			{
+				foreach($concept_positivo as $cp)
+				{
+					// se guardan exclusivamente en otra variable los codigos
+					$y++;
+					$conceptos[$y] =  array('codigo' 	=> $cp->concepto_codigo);
+				}
+			}
+		}
+
+		// se guarda el array conceptos en una variable session
+		Session::put('conceptos',$conceptos);
+
+		return View::make('administracion.recibos.generar_pago')
+			->with('title',$title)
+			->with('generar',$generar);
+	}
+
+	public function post_generar_imprimir()
+	{
+		$title = 'Procesar - Recibos - Sistema Administrativo JG-Sigcon';
+		$fecha = date("d-m-Y");
+		$y = 0;
+		$total = null;
+
+		$generarPay = array('parcela' 	=> Input::get('hparcela'),
+							'ci' 		=> Input::get('hci'),
+							'name' 		=> Input::get('hname'),
+							'monto' 	=> Input::get('monto'),
+							'fecha'		=> $fecha
+					);
+
+		// se recibe el array conceptos
+		$conceptos = Session::get('conceptos');
+		//se cuenta la cantidad de valores que existe en el array
+		$num_con = count($conceptos);
+
+		for($x=1; $x<=$num_con; $x++)
+		{
+			// se recorre el array para buscar el nombre del concepto en la tabla
+			$query_nombre_con = DB::table('tadm_conceptos')
+									->where('codigo','=',$conceptos[$x]['codigo'])
+									->get(array('nombre','monto'));
+
+			foreach($query_nombre_con as $nc)
+			{
+				// se guarda los resultados en otra variable que no sea objeto
+				$y++;
+				$nombre_con[$y] = array('nombre' 	=> $nc->nombre,
+										'monto' 	=> $nc->monto);
+			}
+		}
+
+		// se cuenta la cantidad de valores
+		$num_con = count($nombre_con);
+
+		// se busca cual es el ultimo valor o correlativo de los recibos
+		$correlativo = DB::table('tadm_recibo')->max('id');
+		$correlativo = $correlativo['id'] + 1;
+
+		return View::make('administracion.recibos.generar_imprimir')
+			->with('title',$title)
+			->with('generarPay',$generarPay)
+			->with('nombre_con',$nombre_con)
+			->with('num_con',$num_con)
+			->with('total',$total)
+			->with('correlativo',$correlativo);
 	}
 
 	public function post_bancos()
