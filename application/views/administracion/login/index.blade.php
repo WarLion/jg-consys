4fd51d21073b0936b0169bb873daab7bed4ff985
Juliet Garcia <juliet.garcia@outlook.com>
2013-07-20 11:40:19 -0430

    Densign has been added in the web page.

--- application/views/administracion/login/index.blade.php
+++ application/views/administracion/login/index.blade.php
@@ -1,18 +1,67 @@
-@layout('layouts/admin')
+@layout('layouts/login_consys')
 
 @section('content')
 
-<div class="container">
+<div id="padre">
+
+<div id="hijo">
+<!--<div class="container">-->
 
 	<div class="row">
 
 		<div class="span12">
 
-			C O N T E N I D O 
+			<div style="width: 500px; margin: 0 auto 0 auto;">
+
+				<div style="width: 350px; margin: 0 auto 0 auto;">{{ HTML::image('img/consys/logo_consys.png') }}</div>
+
+				<div>&nbsp;</div>
+
+				<form class="form-horizontal" action='' method="POST">
+
+					<fieldset>
+
+						<div id="legend">
+							<legend class="">Iniciar Sesión</legend>
+						</div>
+
+						<div class="control-group">
+							<!-- Username -->
+							<label class="control-label"  for="username">Usuario</label>
+							<div class="controls">
+								<input type="text" id="username" name="username" placeholder="" class="input-xlarge">
+							</div>
+						</div>
+
+						<div class="control-group">
+							<!-- Password-->
+							<label class="control-label" for="password">Contraseña</label>
+							<div class="controls">
+								<input type="password" id="password" name="password" placeholder="" class="input-xlarge">
+							</div>
+						</div>
+
+
+						<div class="control-group">
+							<!-- Button -->
+							<div class="controls">
+								<button class="btn btn-success">Entrar</button>
+							</div>
+						</div>
+
+					</fieldset>
+
+				</form>
+
+			</div>
 
 		</div>
 
 	</div>
+
+<!--</div>-->
+
+</div>
 
 </div>
 
