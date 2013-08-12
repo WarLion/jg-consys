b537fd443a6f900c5c313a973ef35284a49b50bd
Juliet Garcia <juliet.garcia@outlook.com>
2013-08-12 02:55:24 -0430

    To show owners with more debts and statistics.

--- application/views/administracion/index.blade.php
+++ application/views/administracion/index.blade.php
@@ -2,25 +2,64 @@
 
 @section('content')
 
-<div id="padre" style="width:500px;height:300px;margin:0 auto;display:table;">
+<?php 	
+	Asset::add('highcharts','consys/js/highcharts/js/highcharts.js');
+	Asset::add('exporting','consys/js/highcharts/js/modules/exporting.js'); 
+?>
 
-	<div id="hijo">
+<div class="container">
 
-		<div style="width: 700px; margin: 0 auto 0 auto;">
+	<div class="row-fluid">
 
-		<!--<div class="container">-->
+		<div>
 
-			<div class="row">
+		<div class="span12">
 
-				<div class="span12">
+			<div class="span6">
+				<h4 align="left">Propietarios con más deudas</h4> <hr />
 
-					{{ HTML::image('img/consys/logo_consys.png') }}
+				<table class="table table-hover">
 
-				</div>
+			      <thead>
+			        <tr class="well">
+			          <th>#</th>
+			          <th>Parcela</th>
+			          <th>Propietario</th>
+			          <th>Monto</th>
+			        </tr>
+			      </thead>
 
+			      <tbody>
+			      	@if(!empty($ctasxcobrar))
+			        	@foreach($ctasxcobrar as $cxc)
+				        	<tr>
+								<td>{{ $x++; }}</td>
+								<td>{{ $cxc->parcela_nro }}</td>
+								<td>{{ $cxc->nombre }}</td>
+								<td>Bs. {{ $cxc->monto.',00' }}</td>
+							</tr>
+						@endforeach
+					@else
+			        	<tr>
+							<td>---</td>
+							<td>---</td>
+							<td>---</td>
+							<td>---</td>
+						</tr>
+					@endif
+			      </tbody>
+
+			    </table>
+				
 			</div>
 
-		<!--</div>-->
+			<div class="span6">
+				<h4 align="left">Estadísticas</h4> <hr />
+
+				<div id="graph" style="width: 590px; height: 400px; margin: 0 auto"></div>
+			</div>
+
+		</div>
 
 		</div>
 
@@ -28,5 +67,52 @@
 
 </div>
 
+@endsection
+
+@section('postscript')
+
+<script type="text/javascript">
+	$(function () {
+        $('#graph').highcharts({
+            title: {
+                text: 'Deuda General Total por Mes',
+                x: -20 //center
+            },
+            subtitle: {
+                text: 'Año 2013',
+                x: -20
+            },
+            xAxis: {
+                categories: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
+                    'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
+            },
+            yAxis: {
+                title: {
+                    text: 'Monto (Bs.)'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                valueSuffix: ' Bs'
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+                name: 'Monto',
+                data: [4560, 5789, 3600, 6800, 7012, 5160, 7321, 8600, 0, 0, 0, 0],
+                pointStart: 0
+            }]
+        });
+    });
+    
+</script>
 
 @endsection
